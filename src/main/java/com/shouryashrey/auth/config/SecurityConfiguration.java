8b8a1cf015c761ccf824ab7344556761ef49033c
Shrey Shourya <shrey.shourya3@jublfood.com>
2023-05-08 23:34:13 +0530

    initial commit

---
+++ src/main/java/com/shouryashrey/auth/config/SecurityConfiguration.java
@@ -0,0 +1,12 @@
+package com.shouryashrey.auth.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSec urity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+}
